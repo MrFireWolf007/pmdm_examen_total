bad47ee9668cf9bd390061367fb5e8dd34bfb741
MrFireWolf007 <cristianmorenillapina2001@gmail.com>
2026-02-02 14:33:48 +0100

    Navegacion: Drawer creado

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -14,9 +14,29 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Proyecto base PMDM'),
       ),
+      // El Drawer siempre va en el Scaffold y para que tenga el icono de menu existir un AppBar
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menú principal con un Drawer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              )
+            )
+          ],
+        )
+      ),
       body: const Center(
         child: Text(
-          'Todo listo 🚀',
+          'Todo listo en el Home🚀',
           style: TextStyle(fontSize: 18),
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pmdm_examen_total/main.dart';
 import 'package:pmdm_examen_total/my_app.dart';
 
 void main() {
